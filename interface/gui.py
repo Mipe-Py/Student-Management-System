0860c5402601d8939897b1734e3cb87be910287a
MoonHowl-Py <pecenka.miroslav@gmail.com>
2024-07-22 16:13:40 +0200

    Create gui.py
    menu layout

---
+++ interface/gui.py
@@ -0,0 +1,6 @@
+def menu():
+      """Title and menu"""
+      print("=== Welcome in Student Management System === ")
+      print("1. ADD student\n"
+            "2. SHOW Database\n"
+            "3. EXIT\n")
